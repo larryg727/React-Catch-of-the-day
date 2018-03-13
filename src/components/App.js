9ff9a1468b3dd27bb99e0c8a9dcc1ae772df4fef
larryg727 <larrygonzales727@gmail.com>
2018-03-12 23:30:43 -0500

    Created create fish form and handler funciton

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // TODO: take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // TODO: add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // TODO: set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
